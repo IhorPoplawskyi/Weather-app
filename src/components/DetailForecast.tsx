d17689e74734da7b3fae86f9276073457aff4140
Ihor_Poplavskyi <poplawskyiihor@gmail.com>
2022-11-29 22:27:52 +0200

    added info to details

--- src/components/DetailForecast.tsx
+++ src/components/DetailForecast.tsx
@@ -7,16 +7,26 @@ const DetailForecast: React.FC = () => {
     return (
         <div className={s.hourlyForecastContainer}>
             <div className={s.hourlyForecastContainerInfo}>
-                <div>{new Date(details![0].dt * 1000).toLocaleDateString()}</div>
-                <img src={`../icons/${details![0].weather[0].icon}.png`}/>
-                <div>{Math.round(details![0].main.temp)}°C</div>
+                <div className={s.hourlyForecastContainerInfoItem1}>
+                    <div>{new Date(details![0].dt * 1000).toLocaleDateString()}</div>
+                    <img src={`../icons/${details![0].weather[0].icon}.png`} />
+                    <div>{Math.round(details![0].main.temp)}°C</div>
+                </div>
+                <div className={s.hourlyForecastContainerInfoItem2}>
+                    <div className={s.itemOfInfoItem}>temp</div>
+                    <div className={s.itemOfInfoItem}>feels like</div>
+                    <div className={s.itemOfInfoItem}>pressure</div>
+                    <div className={s.itemOfInfoItem}>humidity</div>
+                    <div className={s.itemOfInfoItem}>wind</div>
+                    <div className={s.itemOfInfoItem}>visibility</div>
+                </div>
             </div>
             {details?.map(el => {
-                const time = new Date(el.dt*1000).toLocaleTimeString().slice(0,5);
+                const time = new Date(el.dt * 1000).toLocaleTimeString().slice(0, 5);
                 return (
                     <div key={el.dt} className={s.hourlyForecastContainerItem}>
                         {time}
-                        <img src={`../icons/${el.weather[0].icon}.png`}/>
+                        <img src={`../icons/${el.weather[0].icon}.png`} />
                         <div className={s.itemsOfContainerItem}>{Math.round(el.main.temp)}°C</div>
                         <div className={s.itemsOfContainerItem}>{Math.round(el.main.feels_like)}°C</div>
                         <div className={s.itemsOfContainerItem}>{el.main.pressure} mm</div>
